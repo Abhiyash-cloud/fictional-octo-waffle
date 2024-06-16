1ecdb81a0f828b242af49a98668432d6cd4ab954
Abhiyash Thakur <132641909+Abhiyash-cloud@users.noreply.github.com>
2024-06-17 02:52:32 +0530

    Update a.cmd

--- a.cmd
+++ a.cmd
@@ -1,7 +1,7 @@
 net user /add Admin P@ssw0rd! 
 net localgroup administrators /add Admin
 choco install ngrok -y
-ngrok config add-authtoken 2dumqfNwOrYeLZWxq5jCHAJvnq2_3SSomQfvvbASdUDZiQMT
+ngrok config add-authtoken 2PMg0hSL9wjdp5VZaDndvEliNf5_VGe7WYHSTKANoMZ17Fvg
 powershell -command D:\a\fictional-octo-waffle\fictional-octo-waffle\tcp.ps1
 sleep 240
 
